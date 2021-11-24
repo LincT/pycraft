767f8dbd93a6bd1a7b1e81993114675d67b6e836
linct <test@testserver.com>
2021-11-23 21:59:00 -0600

    ip validation working

--- app/get_status.py
+++ app/get_status.py
@@ -2,31 +2,49 @@
 
 import requests
 import json
+from ipaddress import ip_address
+
+# TODO: caching
 
 
-def get_public_ip(v: int = 4) -> str:
-    url = {
-        4: "http://ipv4.icanhazip.com",
-        6: "http://ipv6.icanhazip.com"
-    }
-    ip = requests.request("get", f"{url[v]}").text
-    return ip
+def get_public_ip(v: int = 4) -> dict:
+    # TODO: branch ip
+    try:
+        # should add check to verify formatting
+        url = {
+            4: "http://ipv4.icanhazip.com",
+            6: "http://ipv6.icanhazip.com"
+        }
+        ip = requests.request("get", f"{url[v]}")
+
+        # return {"type": v, "address": ip.text.strip()}
+        if len(str(ip_address(ip.text.strip()))) > 0:
+            return {"type": v, "address": ip.text.strip()}
+        else:
+            return {"type": 0, "address": "0.0.0.0"}
+    except ValueError:
+        return {"type": 0, "address": "0.0.0.0"}
 
 
-def get_status():
-    data = requests.request("get", f"https://api.mcsrvstat.us/2/{get_public_ip(4)}")
+def get_status(ip: str = "127.0.0.1") -> json:
+    # todo: branch status
+    # mcsrvstat seems to support fqdn or ipv4, but not ipv6
+    data = requests.request("get", f"https://api.mcsrvstat.us/2/{ip}")
+    # data = "{success}"
     print(data.text)
     status = json.loads(data.text)
-    print(status['debug'])
-    
-# for key in list(str(each) for each in info.keys()):
-#     print(f"{key}:{str(info.get(key))}")
+    return status
 
 
 def main():
-    get_status()
-    pass
-
+    ip_info = get_public_ip()
+    print(ip_info)
+    if ip_info["type"] == 4:
+        status = get_status(ip_info["address"])
+        print(status)
+    else:
+        raise ValueError
+    
 
 if __name__ == '__main__':
     main()
