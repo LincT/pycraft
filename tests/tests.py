5f2d941ebaf49dc07a4c121d742e2ad10117e2c8
Aaron Frost <aaron@frostmn.com>
2021-11-28 15:49:50 -0600

    restructured imports

--- tests/tests.py
+++ tests/tests.py
@@ -2,12 +2,14 @@ import unittest
 from app import world_backup as wb
 
 
+
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         # self.assertEqual(True, False)
         # print(f"root = {wb.script_root()}")
         # print("nothing configured to test")
         print(f"root = {wb.ROOT}")
+
 
 if __name__ == '__main__':
     unittest.main()
